9f2b91f144925228c66c11b487eb0e5662706f99
joshuamakula <makulajosh@gmail.com>
2022-07-19 19:10:12 +0300

    Show cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Navbar from './components/navbar'
+import Amazon from './components/amazon'
+const App = () => {
 
-function App() {
+  const [show, setShow] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar setShow={setShow} />
+      {show ? <Amazon /> : "Cart"} 
     </div>
   );
-}
+};
 
-export default App;
+export default App; 
---
+++ src/components/amazon.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import list from '../data';
+import Card from './Card';
+import '../styles/amazon.css';
+
+const Amazon = () => {
+
+    const [cart, setCart] = useState([]);
+
+    const handleClick = (item) => {
+        cart.push(item)
+        // setCart(...[cart,item]);
+        console.log(cart);
+    }
+
+    return (
+        <section>
+            {list.map((item) => 
+                <Card key={item.id} item={item} handleClick={handleClick} />
+            )}
+        </section>
+    );
+};
+
+export default Amazon;
