e437795cc800d1732da607f5f0aa524a6936eaa6
joshuamakula <makulajosh@gmail.com>
2022-07-19 20:21:59 +0300

    Increment or decrement quantity

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './components/navbar'
 import Amazon from './components/amazon'
+import Cart from './components/Cart';
 const App = () => {
 
   const [show, setShow] = useState(true)
-
-  
   const [cart, setCart] = useState([]);
 
   const handleClick = (item) => {
-      cart.push(item)
-      // setCart(...[cart,item]);
-      console.log(cart);
+    if (cart.indexOf(item) !== -1) return;
+    // cart.push(item)
+    setCart([...cart,item]);
   }
+
+  const handleChange = (item, d) => {
+    const ind = cart.indexOf(item);
+    const arr = cart;
+    arr[ind].amount += d;
+
+    if (arr[ind].amount === 0) arr[ind].amount = 1;
+    setCart([...arr]);
+  }
+
 
   return (
     <div>
-      <Navbar setShow={setShow} />
-      {show ? <Amazon handleClick={handleClick} /> : "Cart"} 
+      <Navbar setShow={setShow} size={cart.length} />
+      {show ? (
+        <Amazon handleClick={handleClick} />
+      ) : (
+        <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
+      ) } 
     </div>
   );
 };
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import '../styles/navbar.css';
 
-const navbar = ({setShow}) => {
+const navbar = ({setShow, size}) => {
     return (
         <div>
             <nav>
                 <div className="nav_box">
-                    <span className="my_shop" onClick={() => setShow(true)} >My Shopping </span>
+                    <span className="my_shop" onClick={() => setShow(true)} >
+                        My Shopping
+                    </span>
                     <div className="cart" onClick={() => setShow(false)} >
                         <span>
                             <i class="fa fa-cart-plus"></i>
                         </span>
-                        <span>0</span>
+                        <span>{size} </span>
                     </div>
                 </div>
             </nav>
